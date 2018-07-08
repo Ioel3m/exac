1f31bdafa0860b637faae110f9339c25dd928ff2
ioel3m <jl3moran@gmail.com>
2018-07-07 22:37:02 -0500

    Correccion de errores en LOGIN, instalacion de JQUERY

--- fronted/src/app/component/login/login.component.ts
+++ fronted/src/app/component/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from "../../services/api.service";
+import * as $ from 'jquery';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,7 +14,42 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
-    this._apiService.getUsuarios('1204392032-EST', '1204392032').subscribe(res=>console.log(res.json()))
+    $(document).ready(function () {
+      $('#i-login').focus(function () {
+          $('#s-login').addClass('i-seleccionado')
+      })
+      $('#i-pass').focus(function () {
+          $('#s-pass').addClass('i-seleccionado')
+      })
+      $('#i-pass').focusout(function () {
+          $('#s-pass').removeClass('i-seleccionado')
+      })
+      $('#i-login').focusout(function () {
+          $('#s-login').removeClass('i-seleccionado')
+      })
+  
+  
+      $('#up-contacto').click(function () {
+          $('#seccion-contacto').animate({ top: '0' }, 600, 'swing');
+          $("#form-contact")[0].reset();
+          $("#form-contacto").removeClass('d-none');
+          $("#form-contacto").addClass('d-block');
+          $(this).hide();
+          $('#down-contacto').show();
+      })
+  
+      $('#down-contacto').click(function () {
+          $('#seccion-contacto').animate({ top: '90vh' }, 600, 'swing');
+          $('#up-contacto').show();
+          $('#down-contacto').hide();
+      })
+  
+  
+  
+  
+  })
+   
+
   }
 
 }
